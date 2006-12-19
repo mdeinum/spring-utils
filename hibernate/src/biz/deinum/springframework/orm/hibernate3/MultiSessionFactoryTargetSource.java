8ab25422a3c030d986fd07cfc8ff8c1d1c30d74d
Marten Deinum <mdeinum@gmail.com>
2006-12-19 22:27:36 +0000

    Added logging, some minor refactoring.

--- hibernate/src/biz/deinum/springframework/orm/hibernate3/MultiSessionFactoryTargetSource.java
+++ hibernate/src/biz/deinum/springframework/orm/hibernate3/MultiSessionFactoryTargetSource.java
@@ -7,78 +7,85 @@ import java.util.Map;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.aop.TargetSource;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.orm.toplink.SessionFactory;
+import org.springframework.util.Assert;
 
 import biz.deinum.springframework.core.ContextHolder;
 
 /**
  * Create multiple SessionFactories and return the correct one for the current
- * company.
+ * context.Very useful if we have one application which points to different
+ * databases, for different context (i.e. clients).
  * 
- * @author M. Deinum
+ * @author Marten Deinum
  * @see TargetSource
  */
 
-public class MultiSessionFactoryTargetSource implements TargetSource {
+public class MultiSessionFactoryTargetSource implements TargetSource, InitializingBean {
 
-        private Logger logger = LoggerFactory.getLogger(MultiSessionFactoryTargetSource.class); 
+	private Logger logger = LoggerFactory
+			.getLogger(MultiSessionFactoryTargetSource.class);
 
-        private String defaultSessionFactory = "ctb";
+	private SessionFactory defaultSessionFactory;
 
-        private final Map sessionFactories = Collections.synchronizedMap(new HashMap());
+	private final Map sessionFactories = Collections
+			.synchronizedMap(new HashMap());
 
-        /**
-         * Sets the map of SessionFactory objects. Clear the map first and then
-         * putAll the sessionFactories in the map. We do this because we want to
-         * keep the synchronized map.
-         * 
-         * @param sessionFactoryMap
-         *            the SessionFactory map.
-         */
-        public void setSessionFactories(final Map sessionFactories) {
-            this.sessionFactories.clear();
-            this.sessionFactories.putAll(sessionFactories);
-        }
+	/**
+	 * Sets the map of SessionFactory objects. Clear the map first and then
+	 * putAll the sessionFactories in the map. We do this because we want to
+	 * keep the synchronized map.
+	 * 
+	 * @param sessionFactoryMap
+	 *            the SessionFactory map.
+	 */
+	public void setSessionFactories(final Map sessionFactories) {
+		this.sessionFactories.clear();
+		this.sessionFactories.putAll(sessionFactories);
+	}
 
-        /**
-         * Locate and return the sessionfactory for the current context.
-         * 
-         * First we lookup the context name from the
-         * <code>ContextPathHolder</code> this context name is used to lookup
-         * the desired sessionfactory. When none is found we return the default
-         * sessionfactory, which should always be configured!
-         * 
-         * @see ContextPathHolder
-         */
-        public Object getTarget() throws Exception {
-            // Determine the current context name from theclass that holds the
-            // context name for the current thread.
-            String contextName = ContextHolder.getContext();
-            if (logger.isDebugEnabled()) {
-                logger.debug("Current context: '" + contextName + "'");
-            }
-            
-            Object sessionObject = sessionFactories.get(contextName);
-            if (sessionObject == null) {
-                sessionObject = sessionFactories.get(defaultSessionFactory);
-                logger.debug("Returning default session factory.");
-            }
-            return sessionObject;
-        }
+	/**
+	 * Locate and return the sessionfactory for the current context.
+	 * 
+	 * First we lookup the context name from the <code>ContextPathHolder</code>
+	 * this context name is used to lookup the desired sessionfactory. When none
+	 * is found we return the default sessionfactory, which must be
+	 * configured!
+	 * 
+	 * @see ContextPathHolder
+	 */
+	public Object getTarget() throws Exception {
+		// Determine the current context name from theclass that holds the
+		// context name for the current thread.
+		String contextName = ContextHolder.getContext();
+		logger.debug("Current context: '{}'", contextName);
 
-        public Class getTargetClass() {
-            return SessionFactory.class;
-        }
-        
-        public boolean isStatic() {
-            return false;
-        }
+		Object sessionObject = sessionFactories.get(contextName);
+		if (sessionObject == null) {
+			sessionObject = defaultSessionFactory;
+			logger.debug("Returning default session factory.");
+		}
+		return sessionObject;
+	}
 
-        public void releaseTarget(Object arg0) throws Exception {
-            // no-op
-        }
-        
-        public void setDefault(final String defaultSessionFactory) {
-            this.defaultSessionFactory=defaultSessionFactory;
-        } 
+	public Class getTargetClass() {
+		return SessionFactory.class;
+	}
+
+	public boolean isStatic() {
+		return false;
+	}
+
+	public void releaseTarget(Object arg0) throws Exception {
+		// no-op
+	}
+
+	public void setDefaultSessionFactory(final SessionFactory defaultSessionFactory) {
+		this.defaultSessionFactory = defaultSessionFactory;
+	}
+
+	public void afterPropertiesSet() throws Exception {
+		Assert.notNull(defaultSessionFactory, "The defaultSessionFactory must be set!");
+	}
 }
