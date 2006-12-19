546678d813d268c35b95a4f5c8acc05aa5928a79
Marten Deinum <mdeinum@gmail.com>
2006-12-19 22:27:59 +0000

    Added logging, some minor refactoring.

--- validation/src/biz/deinum/springframework/validation/AbstractSimpleClassMappingValidator.java
+++ validation/src/biz/deinum/springframework/validation/AbstractSimpleClassMappingValidator.java
@@ -1,18 +1,22 @@
 package biz.deinum.springframework.validation;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.util.Assert;
 import org.springframework.validation.Errors;
 
+
 /**
- * This class takes two parameters a supportedClass and a field. The supportedClass is the class
- * supported by this validator and the field is the field to validate.
+ * This class takes two parameters a supportedClass and a field. The supportedClass 
+ * is the class supported by this validator and the field is the field to validate.
  * 
- * @author mdeinum
+ * @author Marten Deinum
+ * @version 1.1
  *
  */
 public abstract class AbstractSimpleClassMappingValidator extends AbstractValidator implements InitializingBean {
-
+	private Logger logger = LoggerFactory.getLogger(AbstractSimpleClassMappingValidator.class);
 	private Class supportedClass;
 	protected String field;
 	
@@ -30,10 +34,12 @@ public abstract class AbstractSimpleClassMappingValidator extends AbstractValida
 	}
 	
 	public final void setSupportedClass(final Class supportedClass) {
+		logger.debug("supportedClass={}", supportedClass.getName());
 		this.supportedClass=supportedClass;
 	}
 	
 	public final void setField(final String field) {
+		logger.debug("field={}", field);
 		this.field=field;
 	}
 }
