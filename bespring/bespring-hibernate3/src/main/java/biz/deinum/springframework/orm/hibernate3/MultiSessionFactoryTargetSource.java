dc8e7ab69967aae44c03445f0afe5da59c2665e0
Marten Deinum <mdeinum@gmail.com>
2006-12-19 19:54:48 +0000

    Initial import.

---
+++ bespring/bespring-hibernate3/src/main/java/biz/deinum/springframework/orm/hibernate3/MultiSessionFactoryTargetSource.java
@@ -0,0 +1,84 @@
+package biz.deinum.springframework.orm.hibernate3;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.aop.TargetSource;
+import org.springframework.orm.toplink.SessionFactory;
+
+import biz.deinum.springframework.core.ContextHolder;
+
+/**
+ * Create multiple SessionFactories and return the correct one for the current
+ * company.
+ * 
+ * @author M. Deinum
+ * @see TargetSource
+ */
+
+public class MultiSessionFactoryTargetSource implements TargetSource {
+
+        private static final Log logger = LogFactory.getLog(MultiSessionFactoryTargetSource.class); 
+
+        private String defaultSessionFactory = "ctb";
+
+        private final Map sessionFactories = Collections.synchronizedMap(new HashMap());
+
+        /**
+         * Sets the map of SessionFactory objects. Clear the map first and then
+         * putAll the sessionFactories in the map. We do this because we want to
+         * keep the synchronized map.
+         * 
+         * @param sessionFactoryMap
+         *            the SessionFactory map.
+         */
+        public void setSessionFactories(final Map sessionFactories) {
+            this.sessionFactories.clear();
+            this.sessionFactories.putAll(sessionFactories);
+        }
+
+        /**
+         * Locate and return the sessionfactory for the current context.
+         * 
+         * First we lookup the context name from the
+         * <code>ContextPathHolder</code> this context name is used to lookup
+         * the desired sessionfactory. When none is found we return the default
+         * sessionfactory, which should always be configured!
+         * 
+         * @see ContextPathHolder
+         */
+        public Object getTarget() throws Exception {
+            // Determine the current context name from theclass that holds the
+            // context name for the current thread.
+            String contextName = ContextHolder.getContext();
+            if (logger.isDebugEnabled()) {
+                logger.debug("Current context: '" + contextName + "'");
+            }
+            
+            Object sessionObject = sessionFactories.get(contextName);
+            if (sessionObject == null) {
+                sessionObject = sessionFactories.get(defaultSessionFactory);
+                logger.debug("Returning default session factory.");
+            }
+            return sessionObject;
+        }
+
+        public Class getTargetClass() {
+            return SessionFactory.class;
+        }
+        
+        public boolean isStatic() {
+            return false;
+        }
+
+        public void releaseTarget(Object arg0) throws Exception {
+            // no-op
+        }
+        
+        public void setDefault(final String defaultSessionFactory) {
+            this.defaultSessionFactory=defaultSessionFactory;
+        } 
+}
---
+++ bespring/bespring-webflow/src/main/java/biz/deinum/springframework/webflow/App.java
@@ -0,0 +1,13 @@
+package biz.deinum.springframework.webflow;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
