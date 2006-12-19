bc326a4c4ac1c95141ad10057eb3ca5a71b6ee9a
Marten Deinum <mdeinum@gmail.com>
2006-12-19 22:27:48 +0000

    Added logging, some minor refactoring.

--- core/src/biz/deinum/springframework/core/ContextHolder.java
+++ core/src/biz/deinum/springframework/core/ContextHolder.java
@@ -1,9 +1,21 @@
 package biz.deinum.springframework.core;
 
+import org.slf4j.LoggerFactory;
+
+/**
+ * ContextHolder will hold a reference to a context which can be used through-out
+ * the application. The reference will be stored in a <code>ThreadLocal</code>.
+ * 
+ * @author Marten Deinum
+ * @version 1.0
+ *
+ */
 public abstract class ContextHolder {
+
 	private static final ThreadLocal holder = new ThreadLocal();
 	
 	public static void setContext(String context) {
+		LoggerFactory.getLogger(ContextHolder.class).debug("context set '{}'", context);
 		holder.set(context);
 	}
 	
