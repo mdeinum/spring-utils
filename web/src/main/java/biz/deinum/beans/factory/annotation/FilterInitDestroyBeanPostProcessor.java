a4c6422569f146832d2ea13933d9f14afded1146
Marten Deinum <mdeinum@gmail.com>
2016-07-19 08:53:23 +0200

    Update and add copyright headers

--- web/src/main/java/biz/deinum/beans/factory/annotation/FilterInitDestroyBeanPostProcessor.java
+++ web/src/main/java/biz/deinum/beans/factory/annotation/FilterInitDestroyBeanPostProcessor.java
@@ -1,4 +1,26 @@
+/*
+ *  Copyright 2007-2016 the original author or authors.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
 package biz.deinum.beans.factory.annotation;
+
+import java.util.Enumeration;
+import javax.servlet.Filter;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -9,12 +31,6 @@ import org.springframework.context.EnvironmentAware;
 import org.springframework.core.env.Environment;
 import org.springframework.web.context.ServletContextAware;
 import org.springframework.web.filter.GenericFilterBean;
-
-import javax.servlet.Filter;
-import javax.servlet.FilterConfig;
-import javax.servlet.ServletContext;
-import javax.servlet.ServletException;
-import java.util.Enumeration;
 
 /**
  * {@code BeanPostProcessor} implementation for {@code javax.servlet.Filter} instances in the context. Will call the {@code Filter#init}
--- web/src/main/java/biz/deinum/web/servlet/SubDomainLocaleResolver.java
+++ web/src/main/java/biz/deinum/web/servlet/SubDomainLocaleResolver.java
@@ -1,3 +1,19 @@
+/*
+ *  Copyright 2007-2016 the original author or authors.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
 package biz.deinum.web.servlet;
 
 import java.util.Locale;
@@ -8,7 +24,11 @@ import org.springframework.util.StringUtils;
 import org.springframework.web.servlet.i18n.AbstractLocaleResolver;
 
 /**
- * @author marten
+ * {@code LocaleResolver} for resolving the locale based on a subdomain. Assumes that
+ * the first part of the servername is the locale to use i.e. {@code en.mysite.org} will
+ * render the request resource in English.
+ *
+ * @author Marten Deinum
  */
 public class SubDomainLocaleResolver extends AbstractLocaleResolver {
 
